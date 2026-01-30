4e1db729cf1744f711c30bbaf8db1d1aa7904861
Jose Marques <joserobertodasilvamarques@gmail.com>
2026-01-30 13:26:33 -0300

    dia15

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import Catalogo from "./ui/Catalogo";
-
-const produtos = [
-  { id: 1, nome: "camiseta", preco: 49.9 },
-  { id: 2, nome: "Bone", preco: 29.9 },
-  { id: 3, nome: "Tenis", preco: 199.9 },
-];
-
+import Contador from "./components/Contador";
 function App() {
   return (
-    <div className="container">
-      <h1>Catálogo</h1>
-      <Catalogo produtos={produtos} />
+    <div>
+      <Contador />
     </div>
   );
 }
-
 export default App;
--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -3,16 +3,21 @@ import { useState } from "react";
 function Contador() {
   const [numero, setNumero] = useState(0);
 
+  function aumentar() {
+    setNumero(numero + 1);
+  }
+  function diminuir() {
+    setNumero(numero - 1);
+  }
+
   return (
     <div>
       <h2>Contador</h2>
-
       <p>Número: {numero}</p>
 
-      <button onClick={() => setNumero(numero + 1)}>+</button>
-      <button onClick={() => setNumero(numero - 1)}>-</button>
+      <button onClick={aumentar}>+</button>
+      <button onClick={diminuir}>-</button>
     </div>
   );
 }
-
 export default Contador;
