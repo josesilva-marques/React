53b37c57e3905786fd3d7ae0d13b660d5400f870
Jose Marques <joserobertodasilvamarques@gmail.com>
2026-02-01 13:53:14 -0300

    dia17

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import Formulario from "./components/Formulario";
+import { useState } from "react";
+
 function App() {
+  const [mostrar, setMostrar] = useState(false);
+
   return (
     <div>
-      <Formulario />
+      <button onClick={() => setMostrar(!mostrar)}>Mostrar/Ocultar</button>
+      {mostrar && <p>Este texto aparece e some ):</p>}
     </div>
   );
 }
+
 export default App;
