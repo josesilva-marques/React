521bb400da13c8901ba35d875dbdb0634cf963d7
Jose Marques <joserobertodasilvamarques@gmail.com>
2026-02-02 14:01:33 -0300

    dia18

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
 
 function App() {
-  const [mostrar, setMostrar] = useState(false);
+  const [produtos] = useState([
+    { id: 1, nome: "Arroz", preco: 20 },
+    { id: 2, nome: "Feijão", preco: 10 },
+    { id: 3, nome: "Macarrão", preco: 8 },
+  ]);
 
   return (
     <div>
-      <button onClick={() => setMostrar(!mostrar)}>Mostrar/Ocultar</button>
-      {mostrar && <p>Este texto aparece e some ):</p>}
+      <h1>Lista de Produtos</h1>
+      <ul>
+        {produtos.map((produto) => (
+          <li key={produto.id}>
+            {produto.nome} - R$ {produto.preco}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
-
 export default App;
