95f5b68f3eec98c86c2ce3391c249d90fb271f8f
Jose Marques <joserobertodasilvamarques@gmail.com>
2026-02-12 14:44:27 -0300

    dia28

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
+import React, { useEffect } from "react";
+
 import ExibirNumero from "./components/ExibirNumero";
 import BotaoAumentar from "./components/BotaoAumentar";
 import BotaoDiminuir from "./components/BotaoDiminuir";
 
 function App() {
   const [numero, setNumero] = useState(0);
+
+  useEffect(() => {
+    if (numero === 5) {
+      alert("Você chegou em 5!");
+    }
+  }, [numero]);
 
   function aumentar() {
     setNumero(numero + 1);
