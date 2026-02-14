1262804488c87d1f4c7d071350952d886c81ad52
Jose Marques <joserobertodasilvamarques@gmail.com>
2026-02-14 14:01:09 -0300

    dia30

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,49 @@ import { useState, useEffect } from "react";
 function App() {
   const [produtos, setProdutos] = useState([]);
   const [carregando, setCarregando] = useState(true);
+  const [erro, setErro] = useState(null);
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then((response) => response.json())
-      .then((data) => {
+    const buscarProdutos = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+
+        if (!response.ok) {
+          throw new Error("Erro ao buscar produtos: ");
+        }
+
+        const data = await response.json();
         setProdutos(data);
+      } catch (error) {
+        setErro(error.message);
+      } finally {
         setCarregando(false);
-      })
-      .catch(() => setCarregando(false));
+      }
+    };
+
+    buscarProdutos();
   }, []);
+
   return (
     <div>
       <h1>Lista de Produtos</h1>
-      {carregando && <p>Carregando...</p>}
-      {produtos.map((produto) => (
-        <div
-          key={produto.id}
-          style={{ border: "1px solid #ccc", margin: "10px", padding: "10px" }}
-        >
-          <h3>{produto.title}</h3>
-          <p>R$ {produto.price}</p>
-        </div>
-      ))}
+      {carregando && <p>Carregando produtos...</p>}
+      {erro && <p style={{ color: "red" }}>{erro}</p>}
+      {!carregando &&
+        !erro &&
+        produtos.map((produto) => (
+          <div
+            key={produto.id}
+            style={{
+              border: "1px solid #ccc",
+              margin: "10px",
+              padding: "10px",
+            }}
+          >
+            <h3>{produto.title}</h3>
+            <p>R$ {produto.price}</p>
+          </div>
+        ))}
     </div>
   );
 }
