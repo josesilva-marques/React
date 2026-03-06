11611fe51eb9edc7c36602f33c240cd7ab994a4d
Jose Marques <joserobertodasilvamarques@gmail.com>
2026-03-06 18:54:41 -0300

    dia47

--- src/hooks/useAdicionarCarrinho.js
+++ src/hooks/useAdicionarCarrinho.js
@@ -1,8 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { adicionarCarrinho } from "../services/produtoService";
 
 export function useAdicionarCarrinho() {
   return useMutation({
     mutationFn: adicionarCarrinho,
+
+    onMutate: async (novoProduto) => {
+      console.log("Produto adicionado otimisticamente", novoProduto);
+    },
+    onError: (error) => {
+      console.log("Erro ao adicionar produto", error);
+    },
+    onSucess: () => {
+      console.log("Produto confirmado no carrinho");
+    },
   });
 }
